102dbc9c80be48340ebeed9b75c25e6e26c9a49b
Dries.Vanbilloen <dries.vanbilloen@amplexor.com>
2020-12-19 16:56:12 +0100

    Adding other pages and fixing the pop up

--- style/js/index.js
+++ style/js/index.js
@@ -1,23 +1,40 @@
-(function (window, document){
+(function (window, document) {
     "use strict";
-    
+
     function init() {
         _initStickyHeader();
+        _initPopupClose();
     }
-    
+
     function _initStickyHeader() {
-        window.addEventListener('scroll', function() {
+        window.addEventListener('scroll', function () {
             var header = document.querySelector('.dv-main-nav');
             if (this.scrollY > 150) {
-               if (header && !header.classList.contains('dv-main-nav--sticky')) {
-                   header.classList.add('dv-main-nav--sticky');
-               } 
+                if (header && !header.classList.contains('dv-main-nav--sticky')) {
+                    header.classList.add('dv-main-nav--sticky');
+                }
             } else {
-               header.classList.remove('dv-main-nav--sticky');
+                header.classList.remove('dv-main-nav--sticky');
             }
         });
     }
 
+    function _initPopupClose() {
+        var closebutton = document.querySelector('.dv-close-icon svg');
+        var popUp = document.querySelector('.dv-pop-up');
+
+        if (window.sessionStorage.getItem('visited')) {
+            popUp.classList.add('dv-pop-up--hidden');
+        }
+
+        closebutton.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            popUp.classList.add('dv-pop-up--hidden');
+            window.sessionStorage.setItem('visited', true);
+        });
+    }
+
     init();
-    
+
 })(this.window, this.document);
